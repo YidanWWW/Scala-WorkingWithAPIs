a2529ed0c6e9cd72744ab7e8476b80140fe9c821
YidanWWW <wang.yidan2@northeastern.edu>
2025-03-27 12:37:49 -0400

    add

--- spotify/src/main/scala/spotify.scala
+++ spotify/src/main/scala/spotify.scala
@@ -1,35 +1,51 @@
 
 object SpotifyPlaylistAnalysis {
 
-  //send post request following https://developer.spotify.com/documentation/web-api/tutorials/getting-started#request-an-access-token to get the token
-  //this token will expire in 3600s, we have to regenerate token once it is expired
+  //Get this token by following https://developer.spotify.com/documentation/web-api/concepts/access-token
+  //once this token is expired, we have to send post request to get a new one.
   val token = "BQC4WpvHeZ0vuzhbnHVUO6AwqozrpYi7AaY9SlG4IVHE0wKb_xsntyGGUE5q1uCKmqv7xwQ6_OJeiC4d5sx-MldveTCsMbeOm5KVnIn7kVyu6GrgAW3wJn-8ptoH9FBkva1LmS5SCMw"
 
-  val playlistEndpoint = "https://api.spotify.com/v1/playlists/"
+  val playlistEndpoint = "https://api.spotify.com/v1/playlists"
   val artistEndpoint = "https://api.spotify.com/v1/artists/"
+
+  // paginate to retrieve all songs from the playlist.
+  def fetchAllTracks(playlistId: String): Seq[ujson.Value] = {
+    var allTracks = Seq[ujson.Value]()
+    var offset = 0
+    val limit = 100
+    var hasMore = true
+
+    while (hasMore) {
+      val playlistUrl = s"$playlistEndpoint/$playlistId/tracks?limit=$limit&offset=$offset"
+      val playlistResponse = requests.get(
+        playlistUrl,
+        headers = Map("Authorization" -> s"Bearer $token")
+      )
+
+      if (playlistResponse.statusCode == 401) {
+        println("Token is invalid")
+        sys.exit(1)
+      }
+
+      val playlistJson = ujson.read(playlistResponse.text)
+      val items = playlistJson("items").arr
+      allTracks ++= items
+      offset += items.length
+      hasMore = items.length == limit
+    }
+    allTracks
+  }
 
   def main(args: Array[String]): Unit = {
     val playlistId = "5Rrf7mqN8uus2AaQQQNdc1"
-    // Get playlist data in JSON format
-    val playlistUrl = s"$playlistEndpoint$playlistId"
-    val playlistResponse = requests.get(playlistUrl,
-      headers = Map("Authorization" -> s"Bearer $token")
-    )
+    val allTracks = fetchAllTracks(playlistId)
 
-    val playlistJson = ujson.read(playlistResponse.text)
-
-    // Spotify playlist JSON structure:
-    // playlistJson("tracks")("items") is an array where each element contains a "track" object.
-    // Each track object has fields "duration_ms", "name" and "artists" (an array)
-    val tracks = playlistJson("tracks")("items").arr.flatMap { item =>
+    val tracks = allTracks.flatMap { item =>
       val track = item("track")
-      // some tracks may be null, filter them out
       if (track == null) None
       else {
-        // extract song details
         val songName = track("name").str
         val duration = track("duration_ms").num.toLong
-        //each artist object has an "id" and "name"
         val artists = track("artists").arr.map { artist =>
           (artist("name").str, artist("id").str)
         }
@@ -37,7 +53,7 @@ object SpotifyPlaylistAnalysis {
       }
     }
 
-    // Get the top 10 longest songs
+    // top 10 songs
     val top10Songs = tracks.sortBy(-_._2).take(10)
 
     println("Part 1) Top 10 Longest Songs (Song name, duration_ms):")
@@ -45,35 +61,35 @@ object SpotifyPlaylistAnalysis {
       println(s"$name , $duration")
     }
 
-    // Collect all unique artists from these songs
-    val artistMap = scala.collection.mutable.Map[String, String]() // Map artistId -> artistName
+    val artistMap = scala.collection.mutable.Map[String, String]()
     top10Songs.foreach { case (_, _, artists) =>
       artists.foreach { case (name, id) =>
         artistMap.getOrElseUpdate(id, name)
       }
     }
 
-    // For each artist, call the artist endpoint to get follower count
     val artistDetails = artistMap.toSeq.flatMap { case (artistId, name) =>
       try {
         val artistUrl = s"$artistEndpoint$artistId"
-        val artistResponse = requests.get(artistUrl,
+        val artistResponse = requests.get(
+          artistUrl,
           headers = Map("Authorization" -> s"Bearer $token")
         )
+        if (artistResponse.statusCode == 401) {
+          println(s"Get artist $name but token is invalid")
+          sys.exit(1)
+        }
         val artistJson = ujson.read(artistResponse.text)
-        // Followers are in the "followers" field under "total"
         val followers = artistJson("followers")("total").num.toLong
         Some((name, followers))
       } catch {
         case e: Exception =>
-          println(s"Failed to fetch details for artist: $name ($artistId). Reason: ${e.getMessage}")
+          println(s"Get artist $name ($artistId) Information Failure: ${e.getMessage}")
           None
       }
     }
 
-    // Sort the artists by follower count in descending order
     val sortedArtists = artistDetails.sortBy(-_._2)
-
     println("\nPart 2) Artists (sorted by follower count):")
     sortedArtists.foreach { case (name, followers) =>
       println(s"$name : $followers")
